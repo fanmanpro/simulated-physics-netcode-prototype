b2ff4445233500a1542c202a7ebdb366653a0c7e
Hendrik Stephanus du Toit <fanus.link@gmail.com>
2020-01-26 16:54:38 -0600

    removed not used usings

--- Assets/ThreadedNetworkProtocol/TCPClient.cs
+++ Assets/ThreadedNetworkProtocol/TCPClient.cs
@@ -1,12 +1,8 @@
 ﻿using Google.Protobuf;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Net.WebSockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ThreadedNetworkProtocol;
--- Assets/ThreadedNetworkProtocol/UDPClient.cs
+++ Assets/ThreadedNetworkProtocol/UDPClient.cs
@@ -1,16 +1,9 @@
 ﻿using Google.Protobuf;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Sockets;
-using System.Net.WebSockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ThreadedNetworkProtocol;
-using UnityEngine;
 
 public class UDPClient : IClient
 {
