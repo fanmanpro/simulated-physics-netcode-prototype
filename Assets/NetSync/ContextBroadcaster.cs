558a9afd5707de2cb8190cfc602002cb93706d1e
Hendrik Stephanus du Toit <fanus.link@gmail.com>
2020-02-20 01:43:46 -0500

    tick for sim working

--- Assets/NetSync/ContextBroadcaster.cs
+++ Assets/NetSync/ContextBroadcaster.cs
@@ -1,77 +1,76 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Google.Protobuf.WellKnownTypes;
 using ThreadedNetworkProtocol;
+using UnityEngine;
 
 public class ContextBroadcaster : MonoBehaviour
 {
-	public Client client;
-	private List<NetSynced.Rigidbody2D> rigidbodies;
-	public bool Started = false;
+    public Client client;
 
-	// these are in bytes, and 32768 is 32KiB
-	public float MaxPacketSize = 32768;
+    public List<NetSynced.Rigidbody2D> rigidbodies;
 
-	// this can grow depending on how many objects the sim server can
-	// handle to send across to all clients with the current sim
-	public float MaxObjects = 32;
+    // these are in bytes, and 32768 is 32KiB
+    public float MaxPacketSize = 32768;
 
-	public float FullSimulationStatePacketSize = 768;
+    // this can grow depending on how many objects the sim server can
+    // handle to send across to all clients with the current sim
+    public float MaxObjects = 32;
 
-	void FixedUpdate()
-	{
-		prepareContext();
-	}
+    public float FullSimulationStatePacketSize = 768;
 
-	void Start()
-	{
-		rigidbodies = new List<NetSynced.Rigidbody2D>();
-		foreach (GameObject rootGameObject in gameObject.scene.GetRootGameObjects())
-		{
-			rigidbodies.AddRange(rootGameObject.GetComponentsInChildren<NetSynced.Rigidbody2D>());
-		}
-		//StartCoroutine(awaitStart());
-	}
+    void Start()
+    {
+        rigidbodies = new List<NetSynced.Rigidbody2D>();
+        foreach (GameObject
+            rootGameObject
+            in
+            gameObject.scene.GetRootGameObjects()
+        )
+        {
+            rigidbodies
+                .AddRange(rootGameObject
+                    .GetComponentsInChildren<NetSynced.Rigidbody2D>());
+        }
+        //StartCoroutine(awaitStart());
+    }
 
-	//IEnumerator awaitStart()
-	//{
-	//	yield return new WaitUntil(() => { return Started; });
-	//InvokeRepeating("prepareContext", 0, 0.3333f);
-	//}
+    public void PrepareContext()
+    {
+        List<Gamedata.Rigidbody> rigidBodies = new List<Gamedata.Rigidbody>();
+        int offset = 0;
 
-	void prepareContext()
-	{
-		List<Gamedata.Rigidbody> rigidBodies = new List<Gamedata.Rigidbody>();
-		int offset = 0;
-		//for (int r = 0; r < GameServer.RigidbodyByGUID.Count; r++)
-		int r = 0;
-		foreach (NetSynced.Rigidbody2D rb in rigidbodies)
-		{
-			if ((r - offset + 1) * 30 > FullSimulationStatePacketSize)
-			{
-				sendContext(rigidBodies);
-				rigidBodies.Clear();
-				offset = r;
-			}
+        //for (int r = 0; r < GameServer.RigidbodyByGUID.Count; r++)
+        int r = 0;
+        foreach (NetSynced.Rigidbody2D rb in rigidbodies)
+        {
+            if ((r - offset + 1) * 30 > FullSimulationStatePacketSize)
+            {
+                sendContext (rigidBodies);
+                rigidBodies.Clear();
+                offset = r;
+            }
 
-			//if (!rigidbody.IsAwake()) continue;
+            //if (!rigidbody.IsAwake()) continue;
+            rigidBodies.Add(rb.Export());
+            r++;
+        }
+        if (rigidBodies.Count > 0)
+        {
+            sendContext (rigidBodies);
+            rigidBodies.Clear();
+        }
+    }
 
-			rigidBodies.Add(rb.Export());
-			r++;
-		}
-		if (rigidBodies.Count > 0)
-		{
-			sendContext(rigidBodies);
-			rigidBodies.Clear();
-		}
-	}
-	void sendContext(List<Gamedata.Rigidbody> rigidBodies)
-	{
-		Gamedata.Context context = new Gamedata.Context
-		{
-			RigidBodies = { rigidBodies }
-		};
-		client.Send(PacketType.Unreliable, new Gamedata.Packet { OpCode = Gamedata.Header.Types.OpCode.Context, Data = Any.Pack(context) });
-	}
+    void sendContext(List<Gamedata.Rigidbody> rigidBodies)
+    {
+        Gamedata.Context context =
+            new Gamedata.Context { RigidBodies = { rigidBodies } };
+        client
+            .Send(PacketType.Unreliable,
+            new Gamedata.Packet {
+                OpCode = Gamedata.Header.Types.OpCode.Context,
+                Data = Any.Pack(context)
+            });
+    }
 }
--- Assets/ThreadedNetworkProtocol/UDPClient.cs
+++ Assets/ThreadedNetworkProtocol/UDPClient.cs
@@ -1,131 +1,155 @@
-﻿using Google.Protobuf;
-using System;
+﻿using System;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Protobuf;
 using ThreadedNetworkProtocol;
+using UnityEngine;
 
 public class UDPClient : IClient
 {
-	private UdpClient udpClient;
-	private ClientEndPoint remoteEndPoint;
+    private UdpClient udpClient;
 
-	private ClientState clientState;
+    private ClientEndPoint remoteEndPoint;
 
-	private PacketHandler packetHandler;
+    private ClientState clientState;
 
-	public bool Active => clientState.Connected && clientState.Trusted;
+    private PacketHandler packetHandler;
 
-	public UDPClient(int port, ClientEndPoint c, ClientState cs, PacketHandler p)
-	{
-		udpClient = new UdpClient(port);
-		packetHandler = p;
+    public bool Active => clientState.Connected && clientState.Trusted;
 
-		remoteEndPoint = c;
-		clientState = cs;
-	}
+    public UDPClient(
+        int port,
+        ClientEndPoint c,
+        ClientState cs,
+        PacketHandler p
+    )
+    {
+        udpClient = new UdpClient(port);
+        packetHandler = p;
 
-	public async Task<Error> Connect()
-	{
-		if (remoteEndPoint == null) return new Error("[UDP] Client remote end point not provided");
-		if (clientState.Connected) return new Error("[UDP] Client already connected");
-		if (clientState.Connecting) return new Error("[UDP] Client already busy connecting");
-		if (udpClient.Client.Connected) return new Error("[UDP] Client socket open but state not connected");
+        remoteEndPoint = c;
+        clientState = cs;
+    }
 
-		using (CancellationTokenSource cts = new CancellationTokenSource())
-		{
-			clientState.Connecting = true;
-			// Doesn't really do anything. Just establishes a default remote host using the specified network endpoint.
-			udpClient.Connect(remoteEndPoint.IPEndPoint);
-			clientState.Connecting = false;
-			if (!udpClient.Client.Connected) return new Error("[UDP] Client connected but weird that the socket state is stil not connected");
-			// all good
-			clientState.Connected = true;
-			return null;
-		}
-	}
+    public async Task<Error> Connect()
+    {
+        if (remoteEndPoint == null)
+            return new Error("[UDP] Client remote end point not provided");
+        if (clientState.Connected)
+            return new Error("[UDP] Client already connected");
+        if (clientState.Connecting)
+            return new Error("[UDP] Client already busy connecting");
+        if (udpClient.Client.Connected)
+            return new Error("[UDP] Client socket open but state not connected");
 
-	public Error Disconnect()
-	{
-		clientState.Connected = false;
-		clientState.Connecting = false;
-		clientState.Disconnected = false;
-		clientState.Disconnecting = false;
-		clientState.Listening = false;
-		clientState.Transmitting = false;
-		clientState.Trusted = false;
+        using (CancellationTokenSource cts = new CancellationTokenSource())
+        {
+            clientState.Connecting = true;
 
-		udpClient.Close();
-		return null;
-	}
+            // Doesn't really do anything. Just establishes a default remote host using the specified network endpoint.
+            udpClient.Connect(remoteEndPoint.IPEndPoint);
+            clientState.Connecting = false;
+            if (!udpClient.Client.Connected)
+                return new Error("[UDP] Client connected but weird that the socket state is stil not connected");
 
-	//public async Error Send(Packet packet)
-	public async Task<Error> Send(Gamedata.Packet packet)
-	{
-		ArraySegment<byte> sendBuffer = new ArraySegment<byte>(packet.ToByteArray());
-		//if (debug) Debug.Log(string.Format("WS - {0} {1}", "Sending packet", packet.Header.OpCode));
-		using (CancellationTokenSource cts = new CancellationTokenSource())
-		{
-			try
-			{
-				await udpClient.SendAsync(sendBuffer.Array, sendBuffer.Count);
-				//await clientWebSocket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, cts.Token);
-				return null;
-			}
-			catch
-			{
-				return new Error("[UDP] Client failed to send packet");
-			}
-		}
-	}
+            // all good
+            clientState.Connected = true;
+            return null;
+        }
+    }
 
-	public async Task<Error> Listen()
-	{
+    public Error Disconnect()
+    {
+        clientState.Connected = false;
+        clientState.Connecting = false;
+        clientState.Disconnected = false;
+        clientState.Disconnecting = false;
+        clientState.Listening = false;
+        clientState.Transmitting = false;
+        clientState.Trusted = false;
 
-		//if (clientWebSocket.State != WebSocketState.Open)
-		//{
-		//	return new Error("[UDP] Client tried to listen for packets but socket is closed");
-		//}
-		try
-		{
-			while (udpClient.Client.Connected)
-			{
-				UdpReceiveResult receiveBytes = await udpClient.ReceiveAsync();
-				Gamedata.Packet packet = Gamedata.Packet.Parser.ParseFrom(receiveBytes.Buffer);
-				if (packet.OpCode != Gamedata.Header.Types.OpCode.Invalid)
-				{
-					clientState.Trusted = true;
-					packetHandler.Handle(packet);
-				}
-				//	clientState.Listening = true;
+        udpClient.Close();
+        return null;
+    }
 
-				//	byte[] rcvBytes = new byte[1024];
-				//	ArraySegment<byte> rcvBuffer = new ArraySegment<byte>(rcvBytes);
-				//	WebSocketReceiveResult rcvResult = await clientWebSocket.ReceiveAsync(rcvBuffer, CancellationToken.None);
-				//	if (rcvResult.Count <= 0) break;
-				//	//if (debug) Debug.Log(string.Format("WS - {0}", "Receiving packet"));
-				//	if (rcvBytes.Length < rcvResult.Count)
-				//	{
-				//		Debug.LogError("Incoming message aborted because to big. Decrease message size or increase buffer length.");
-				//		continue;
-				//	}
-				//	if (rcvResult.EndOfMessage)
-				//	{
-				//		byte[] msgBytes = rcvBuffer/*.Skip(rcvBuffer.Offset)*/.Take(rcvResult.Count).ToArray();
-				//		Debug.Log(Encoding.UTF8.GetString(msgBytes, 0, msgBytes.Length));
-				//		//Packet packet = Packet.Parser.ParseFrom(msgBytes);
-				//		//HandlePacket(packet);
-				//	}
-			}
-		}
-		catch (Exception n)
-		{
-			clientState.Connecting = false;
-			clientState.Connected = false;
-			clientState.Listening = false;
-			return new Error("[UDP] Client connection was forcibly closed: " + n);
-		}
-		clientState.Listening = false;
-		return null;
-	}
+    //public async Error Send(Packet packet)
+    public async Task<Error> Send(byte[] bytes)
+    {
+        ArraySegment<byte> sendBuffer = new ArraySegment<byte>(bytes);
+
+        //if (debug) Debug.Log(string.Format("WS - {0} {1}", "Sending packet", packet.Header.OpCode));
+        using (CancellationTokenSource cts = new CancellationTokenSource())
+        {
+            try
+            {
+                await udpClient.SendAsync(sendBuffer.Array, sendBuffer.Count);
+
+                //await clientWebSocket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, cts.Token);
+                return null;
+            }
+            catch
+            {
+                return new Error("[UDP] Client failed to send packet");
+            }
+        }
+    }
+
+    public async Task<Error> Send(Gamedata.Packet packet)
+    {
+        return await Send(packet.ToByteArray());
+    }
+
+    public async Task<Error> Listen()
+    {
+        //if (clientWebSocket.State != WebSocketState.Open)
+        //{
+        //	return new Error("[UDP] Client tried to listen for packets but socket is closed");
+        //}
+        try
+        {
+            Debug.Log("receiving");
+            while (udpClient.Client.Connected)
+            {
+                UdpReceiveResult receiveBytes = await udpClient.ReceiveAsync();
+
+                //Gamedata.Packet packet =
+                //    Gamedata.Packet.Parser.ParseFrom(receiveBytes.Buffer);
+                //if (packet.OpCode != Gamedata.Header.Types.OpCode.Invalid)
+                //{
+                clientState.Trusted = true;
+                packetHandler.Handle(null);
+                //}
+                //	clientState.Listening = true;
+
+                //	byte[] rcvBytes = new byte[1024];
+                //	ArraySegment<byte> rcvBuffer = new ArraySegment<byte>(rcvBytes);
+                //	WebSocketReceiveResult rcvResult = await clientWebSocket.ReceiveAsync(rcvBuffer, CancellationToken.None);
+                //	if (rcvResult.Count <= 0) break;
+                //	//if (debug) Debug.Log(string.Format("WS - {0}", "Receiving packet"));
+                //	if (rcvBytes.Length < rcvResult.Count)
+                //	{
+                //		Debug.LogError("Incoming message aborted because to big. Decrease message size or increase buffer length.");
+                //		continue;
+                //	}
+                //	if (rcvResult.EndOfMessage)
+                //	{
+                //		byte[] msgBytes = rcvBuffer/*.Skip(rcvBuffer.Offset)*/.Take(rcvResult.Count).ToArray();
+                //		Debug.Log(Encoding.UTF8.GetString(msgBytes, 0, msgBytes.Length));
+                //		//Packet packet = Packet.Parser.ParseFrom(msgBytes);
+                //		//HandlePacket(packet);
+                //	}
+            }
+        }
+        catch (Exception n)
+        {
+            clientState.Connecting = false;
+            clientState.Connected = false;
+            clientState.Listening = false;
+            return new Error("[UDP] Client connection was forcibly closed: " +
+                n);
+        }
+        clientState.Listening = false;
+        return null;
+    }
 }
